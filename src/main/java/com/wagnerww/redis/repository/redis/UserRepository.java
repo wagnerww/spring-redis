e7f6a9b2d8b7b818cb8bfd94d7693c807f4cd416
wagner <wagnerricardonet@gmail.com>
2021-01-08 12:54:04 -0300

    lint

--- src/main/java/com/wagnerww/redis/repository/redis/UserRepository.java
+++ src/main/java/com/wagnerww/redis/repository/redis/UserRepository.java
@@ -5,11 +5,9 @@ import java.util.Map;
 import com.wagnerww.redis.models.UserModel;
 
 public interface UserRepository {
-
   void save(UserModel userModel);
   Map<String, UserModel> findAll();
   UserModel findById(String id);
   void update(UserModel userModel);
   void deleteById(String id);
-
 }
